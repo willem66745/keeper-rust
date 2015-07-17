9c160c83e34bb14abc1a4d44f9d403f2e7d428ec
Willem <willem66745@gmail.com>
2015-07-17 21:11:30 +0200

    web: apply theme based on switch-state

--- web/res/keeper.js
+++ web/res/keeper.js
@@ -1,12 +1,23 @@
-// Retrieve all switch configurations and update web user interface
-//
-// See: http://demos.jquerymobile.com/1.4.5/listview/
-function load_switches() {
+// Show loader and disable the rest of the user interface
+function show_loader() {
     $("#foo").addClass('ui-disabled');
     $.mobile.loading("show", {
         text: "loading",
         textVisible: true
     });
+}
+
+// Hide loader and activate the rest of the user interface
+function hide_loader() {
+    $.mobile.loading("hide");
+    $("#foo").removeClass('ui-disabled');
+}
+
+// Retrieve all switch configurations and update web user interface
+//
+// See: http://demos.jquerymobile.com/1.4.5/listview/
+function load_switches() {
+    show_loader();
 
     $.post("/api/switches", function(data) {
         var items = data.length;
@@ -15,7 +26,9 @@ function load_switches() {
         $("#content").hide();
 
         $.each(data, function(index, object) {
-            $("#content").append("<li>" + object + "</li>");
+            var li = $("#content").append('<li>' + object + '</li>');
+
+            li = li.find("li").last();
 
             $.ajax({
                 url: "/api/get/" + object,
@@ -23,14 +36,21 @@ function load_switches() {
                 async: false,
                 timeout: 2000
             }).done(function(data) {
-                console.debug(object);
-                console.debug(data);
+                //console.debug(object);
+                //console.debug(data);
+                if (data.switch == true) {
+                    // apply light theme when switch is enabled
+                    li.attr("data-theme", "a");
+                }
+                else {
+                    // apply dark theme when switch is enabled
+                    li.attr("data-theme", "b");
+                }
             }).fail(function() {
             }).always(function() {
                 items--;
                 if (items == 0) {
-                    $.mobile.loading("hide");
-                    $("#foo").removeClass('ui-disabled');
+                    hide_loader();
                 }
             });
         });
